66d21792828ba638382dbb1a9a74ffc1734f1f42
humanova <craxes_humanova@hotmail.com>
2018-10-19 12:30:40 +0300

    added port setting, lined up the gui

--- server.py
+++ server.py
@@ -1,8 +1,19 @@
 import http.server
 import socketserver
+import configparser
+
+def get_port():
+    config = configparser.ConfigParser()
+    try:
+        config.read('capwin.ini')
+        return int(config['Server']['server_port'])
+    except:
+        print("Couldn't get server_port from capwin.ini...")
+        return 8000
+
 Handler = http.server.SimpleHTTPRequestHandler
 
-PORT = 8000
+PORT = get_port()
 
 with socketserver.TCPServer(("0.0.0.0", PORT), Handler) as httpd:
     print("Serving at localhost:" + str(PORT))
